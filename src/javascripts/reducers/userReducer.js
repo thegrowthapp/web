3e2cdbc893b14c44645d28e6fb3b7b8efb9058cc
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-09-06 14:23:27 +0200

    [#4] Redux foundation (#7)
    * [#4] Redux foundation
    
    * fix travis
    
    * alter test script

---
+++ src/javascripts/reducers/userReducer.js
@@ -0,0 +1,30 @@
+export default function reducer(state = {
+  id: null,
+  firstName: null,
+  lastName: null,
+  email: null,
+  app: {
+    fetching: false,
+    fetched: false,
+    error: null
+  }
+}, action) {
+  switch (action.type) {
+  case 'FETCH_USER': {
+    return {...state, app: {fetching: true}}
+  }
+  case 'FETCH_USER_REJECTED': {
+    return {...state, app: {fetching: false, error: action.payload}}
+  }
+  case 'FETCH_USER_FULFILLED': {
+    return {
+      ...state,
+      app: {fetching: false, fetched: true},
+      user: action.payload,
+    }
+  }
+  default: {
+    return state
+  }
+  }
+}
